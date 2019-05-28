0510085c0b816eec0b4b180e0664f8c12441d749
Aleksei <gogohandz@gmail.com>
2019-05-28 15:45:27 +0300

    Add firebase initialization

---
+++ src/components/LoginForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Button, Card, CardSection } from './common';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+
+export default LoginForm;
