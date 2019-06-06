f820935f7f4c031ec852880c46c2702bb6242fdf
Aleksei <gogohandz@gmail.com>
2019-06-06 14:25:40 +0300

    Add registration form

--- src/RegistrationScreen.js
+++ src/RegistrationScreen.js
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
-import LoginForm from './components/LoginForm';
+import { View, TextInput, TouchableOpacity, Text } from 'react-native';
+import { Button, Card, CardSection } from './components/common';
 
 class RegistrationScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: 'Email',
+      password: 'Password',
+      name: 'Your name'
+    };
+  }
+
+  onPress() {
+    const { email, password } = this.state;
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      console.log(errorMessage);
+    });
+
+    console.log(this.state.email);
+  }
 
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <Text>
-          REGISTRATION
-        </Text>
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(name) => this.setState({name})}
+          value={this.state.name}
+        />
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(email) => this.setState({email})}
+          value={this.state.email}
+        />
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(password) => this.setState({password})}
+          value={this.state.password}
+        />
+        <Button style={{ height: 70 }} onPress={this.onPress.bind(this)}>
+          Create account
+        </Button>
       </View>
     );
   }
