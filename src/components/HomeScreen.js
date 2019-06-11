f25c253d1eaa27ce11a5779148bdf0a0ce41e881
Aleksei <gogohandz@gmail.com>
2019-06-11 14:08:56 +0300

    Authentication done with web based Firebase

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './common';
+import { Header, Button } from './common';
 import LoginForm from './LoginForm';
 
 class HomeScreen extends Component {
+  constructor(props) {
+    super();
+    this.state = { loggedIn: null, privet: 1 };
+  }
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: "AIzaSyA3wlEyNLNiLQ0kJiGKTKalLUi_DwGRFTA",
@@ -15,14 +20,75 @@ class HomeScreen extends Component {
       messagingSenderId: "365214593053",
       appId: "1:365214593053:web:abe7bf191a3c84ba"
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      console.log('Mounting...');
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+
+    setTimeout(() => {
+      this.setState({ privet: 2 });
+      console.log('privet');
+      console.log(this.state.loggedIn);
+      console.log(user);
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    }, 11000);
+  }
+
+  // async signOutUser() {
+  //   try {
+  //     await firebase.auth().signOut();
+  //     // navigate('Auth');
+  //     console.log('Succesfully logged out');
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }
+
+  signOutUser() {
+    firebase.auth().signOut()
+      .then(() => {
+        console.log('Sign Out Done');
+      });
+  }
+
+  renderMainScreen() {
+    let self = this;
+    if (this.state.loggedIn) {
+      console.log('LOGGED IN');
+      return (
+        <Button style={{ height: 70 }} onPress={this.signOutUser}>
+          Log Out
+        </Button>
+      );
+    } else if (this.state.loggedIn === false) {
+      console.log('NOT LOGGED IN');
+      return (
+        <LoginForm navigation={self.props.navigation} />
+      );
+    } else {
+      return (
+        <ActivityIndicator />
+      );
+    }
   }
 
   render() {
     const {navigate} = this.props.navigation;
+
     return (
       <View style={{ flex: 1 }}>
         <Header headerText='Authentication' />
-        <LoginForm navigation={this.props.navigation} />
+        { this.renderMainScreen() }
       </View>
     );
   }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,27 +1,71 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
-import { View, TextInput, TouchableOpacity, Text } from 'react-native';
+import { View, TextInput, Text, ActivityIndicator } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: 'Email',
-      password: 'Password'
+      email: 'Email@gmail.com',
+      password: 'Password',
+      error: '',
+      loading: false
     };
   }
 
   onPress() {
     const { email, password } = this.state;
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorMessage);
-    });
 
-    console.log(this.state.email);
+    this.setState({ error: '', loading: true });
+    console.log('Waiting...');
+
+    firebase.auth().signInWithEmailAndPassword(email, password);
+    console.log('Done 1');
+
+    setTimeout(() => {
+      console.log('privet privet');
+      this.onLoginSuccess();
+    }, 7000);
+
+    console.log('Done 2');
+  }
+
+  // onPress() {
+  //   const { email, password } = this.state;
+  //
+  //   this.setState({ error: '', loading: true });
+  //
+  //   firebase.auth().signInWithEmailAndPassword(email, password)
+  //     .then(this.onLoginSuccess.bind(this))
+  //     .catch(this.onLoginFail.bind(this));
+  // }
+
+  onLoginSuccess() {
+    console.log('LOGIN SUCCESS');
+    console.log(this);
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    });
+  }
+
+  onLoginFail() {
+    this.setState({ error: 'Authentication Failed', loading: false });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <ActivityIndicator size="small" />;
+    }
+
+    return (
+      <Button style={{ height: 70 }} onPress={this.onPress.bind(this)}>
+        Log In
+      </Button>
+    );
   }
 
   render() {
@@ -38,9 +82,12 @@ class LoginForm extends Component {
           onChangeText={(password) => this.setState({password})}
           value={this.state.password}
         />
-        <Button style={{ height: 70 }} onPress={this.onPress.bind(this)}>
-          Log In
-        </Button>
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
+        { this.renderButton() }
+
         <Button style={{ height: 70 }} onPress={() => navigate('Registration')}>
           Registation
         </Button>
@@ -48,5 +95,13 @@ class LoginForm extends Component {
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
